f5bf8c38d6745d333b0c166f1db2229bf4183a8a
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-21 16:14:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array
 }
-
+const exclaimfacts = []
 function johnLennonFacts() {
+  const exclaimfacts = []
   const facts = [
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
     "He was a choir boy and boy scout",
     "He hated the sound of his own voice"]
-  const exclaimfacts = []
   let i = 0
   while (i < facts.length) {
     exclaimfacts.push(`${facts[i]}!!!`)
