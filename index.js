096dd3ef834371d15135cd8064ad6da566fc82a2
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-21 16:26:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ function johnLennonFacts() {
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
     "He was a choir boy and boy scout",
-    "He hated the sound of his own voice",
-  "foo", "bar"]
+    "He hated the sound of his own voice"]
   let i = 0
   while (i < facts.length) {
     exclaimfacts.push(`${facts[i]}!!!`)
